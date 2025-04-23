5a0420fb5cf5c5995d47ad0bdb175aae0d1a1abf
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-04-22 22:05:31 -0700

    CHANGE: assertion added to 2 contains block.

--- cypress/e2e/product_details.cy.js
+++ cypress/e2e/product_details.cy.js
@@ -1,14 +1,12 @@
 describe("product_details", () => {
-  before(() => {
+  it("Can see the detail of product on detail page when click each product", () => {
     cy.visit("/");
 
     cy.get(".products > :nth-child(1)").click();
-  });
 
-  it("Can see the detail of product on detail page when click each product", () => {
-    cy.contains("Add");
+    cy.contains("Add").should("be.visible");
     cy.get(".main-img").should("exist");
-    cy.contains("Scented Blade");
+    cy.contains("Scented Blade").should("be.visible");
     cy.get(":nth-child(2) > p").should("exist");
     cy.get(".quantity").should("exist");
     cy.get(".price").should("exist");
